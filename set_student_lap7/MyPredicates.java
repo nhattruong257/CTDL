da9d5cdf823cf9cebf6ece8d09208299851207d2
nhattruong257 <146456284+nhattruong257@users.noreply.github.com>
2023-11-06 22:50:27 +0700

    Add files via upload

---
+++ set_student_lap7/MyPredicates.java
@@ -0,0 +1,53 @@
+package set_student_lap7;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class MyPredicates {
+	// Remove every object, obj, from coll for which p.test(obj)
+	// is true. (This does the same thing as coll.removeIf(p).)
+	public static <T> void remove(Collection<T> coll, Predicate<T> p) {
+		// TODO
+		coll.remove(p);
+	}
+
+	// Remove every object, obj, from coll for which
+	// pr.test(obj) is false. (That is, retain the
+	// objects for which the predicate is true.)
+	public static <T> void retain(Collection<T> coll, Predicate<T> p) {
+		// TODO
+		coll.removeIf(item -> !p.test(item));
+	}
+
+	// Return a Set that contains all unique objects, obj,
+	// from the collection, coll, such that p.test(obj)
+	// is true.
+	public static <T> Set<T> collect(Collection<T> coll, Predicate<T> p) {
+		// TODO
+		Set<T> re = new HashSet<>();
+		for(T item : coll) {
+			if(p.test(item)) {
+				re.add(item);
+			}
+			
+		}
+		return re;
+	}
+
+	// Return the index of the first item in list
+	// for which the predicate is true, if any.
+	// If there is no such item, return -1.
+	public static <T> int find(Collection<T> coll, Predicate<T> p) {
+		// TODO
+		int index = 0;
+		for(T item : coll) {
+			if(p.test(item)) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+}
---
+++ set_student_lap7/MyWordCount.java
@@ -0,0 +1,115 @@
+package set_student_lap7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MyWordCount {
+	// public static final String fileName = "data/hamlet.txt";
+	public static final String fileName = "data/fit.txt";
+
+	private List<String> words = new ArrayList<>();
+
+	public MyWordCount() {
+		try {
+			this.words.addAll(Utils.loadWords(fileName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Prints out the number of times each unique token appears in the file
+	// data/hamlet.txt (or fit.txt)
+	// In this method, we do not consider the order of tokens.
+	public List<WordCount> getWordCounts() {
+		List<WordCount> re = new ArrayList<WordCount>();
+		for (String w : words) {
+			if (!re.contains(w)) {
+				re.add(new WordCount(w, count(w)));
+			}
+		}
+		return re;
+	}
+
+	public int count(String w) {
+		int count = 0;
+		for (String s : words) {
+			if (s.equals(w))
+				count++;
+		}
+		return count;
+	}
+
+	// Returns the words that their appearance are 1, do not consider duplidated
+	// words
+	public Set<String> getUniqueWords() {
+		Set<String> re = new HashSet<String>();
+		for (WordCount wc : getWordCounts()) {
+			if (wc.getCount() ==1)
+				re.add(wc.getWord());
+		}
+		return re;
+	}
+
+	// Returns the words in the text file, duplicated words appear once in the
+	// result
+	public Set<String> getDistinctWords() {
+		Set<String> re = new HashSet<String>();
+		for (WordCount wc : getWordCounts()) {
+			if (wc.getCount() >1)
+				re.add(wc.getWord());
+		}
+		return re;
+	}
+
+	// Prints out the number of times each unique token appears in the file
+	// data/hamlet.txt (or fit.txt) according ascending order of tokens
+	// Example: An - 3, Bug - 10, ...
+	public Set<WordCount> printWordCounts() {
+		Set<WordCount> re = new TreeSet<WordCount>(new Comparator<WordCount>() {
+			
+			@Override
+			public int compare(WordCount wc1, WordCount wc2) {
+				// TODO Auto-generated method stub
+				int re = wc1.getCount() - wc2.getCount();
+				if(re==0) {
+					return wc2.getWord().compareTo(wc1.getWord());
+				}
+				return re;
+			}
+			
+		});
+		re.addAll(this.getWordCounts());
+		return re;
+	}
+	public Set<String> filterWords(String pattern) {
+		// TODO
+		Set<String> filter = new HashSet<String>();
+		for(String word : words)
+		{
+			if(!word.startsWith(pattern)) {
+				filter.add(word);
+			}
+		}
+		return filter;
+		}
+
+	public static void main(String[] args) {
+		MyWordCount mywordcount = new MyWordCount();
+		System.out.println((mywordcount.getWordCounts()));
+		System.out.println((mywordcount.getDistinctWords()));
+		System.out.println((mywordcount.getUniqueWords()));
+		System.out.println((mywordcount.printWordCounts()));
+		System.out.println(mywordcount.filterWords("L"));
+	}
+}
